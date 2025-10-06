9e64dc76e5c8e1ac9c38445791b792a1808a6621
miftahulhdd <miftahulhudaamrii@gmail.com>
2025-10-06 15:03:33 +0700

    fix bug 9

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
     navigator.serviceWorker
-      .register("/service-worker.js")
+      .register("./service-worker.js")
       .then((reg) => console.log("Service Worker registered", reg))
       .catch((err) => console.log("Service Worker registration failed", err));
   });
